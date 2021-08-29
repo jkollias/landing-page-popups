c603bce1b0dae3a2f44b2bc96f0a381ad19a0288
jasonkollias <30324218+jasonkollias@users.noreply.github.com>
2021-08-29 15:35:19 -0400

    Adding close function and creating java script file

---
+++ js/landing-page-popup.js
@@ -0,0 +1,91 @@
+
+//https://www.joahbeauty.com/media/wysiwyg/joah/home-page/rising-up-video.mp4
+//https://www.youtube.com/embed/RWsCXWYXDso
+//.this-is-my-div
+
+
+class LandingPagePopup{
+
+  constructor(content){
+    this.content = content;
+  }
+
+  determineContent(){
+    let popupContent = {};
+    if (this.content.substring(0,1) == "." || this.content.substring(0,1) == "#"){
+      popupContent.type = "html-content"
+      popupContent.source = this.generateHTMLContent(this.content);
+    }else if( this.content.substring(0,24)  == "https://www.youtube.com/"){
+      popupContent.type = "youtube-video";
+      popupContent.source = this.generateYoutubeContent(this.content);
+    }else if(this.content.substring(0,27)  == "https://www.joahbeauty.com/"){
+      popupContent.type = "joah-video";
+    }
+    else{
+      console.error("You must add class, id or video url");
+    }
+    return popupContent;
+  }
+
+  generateHTMLContent(content){
+    let htmlContent = `<section class="html-popup-content--container">`
+    htmlContent+= document.querySelector(content).innerHTML;
+    htmlContent+= `</section>`;
+    return htmlContent;
+  }
+
+  generateYoutubeContent(content){
+    let youtubeContent =`<iframe 
+        id="video-src"
+        class="youtube-video--iframe"
+        width="560" height="315" 
+        src="${content}" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+      </iframe>`;
+    return youtubeContent;
+  }
+
+  createPopupContent(){
+    let popupContent = this.determineContent();
+    let html;
+    html = document.createElement("section");
+    html.classList.add("landing-page--popup-container");
+    html.setAttribute("aria-hidden","false");
+    html.innerHTML = `
+              <div class="inner-${popupContent.type}-popup inner-landing-page--video-popup"> 
+                <button 
+                  class="close-popup--btn" 
+                  onClick="removeLandingPagePopUp(this)">
+                    <div class="close-icon"></div>
+                    <div class="screen-reader-text">Close ${popupContent.type} Button</div> 
+                </button>
+                 
+                <div class="${popupContent.type}-container"  >
+                  <div class="video-loader">
+                    <div class="loading-icon"></div>
+                    <div>Loading...</div>
+                  </div> 
+                  ${popupContent.source}
+                </div><!-- ${popupContent.type}-container -->
+              </div>`;
+      return html;
+  }
+
+  showPopup(){
+    document.body.classList.add('no-scroll');
+    document.body.appendChild( this.createPopupContent() );
+  }
+
+ 
+
+}
+
+
+function removeLandingPagePopUp(btn){
+  let popup = btn.closest(".landing-page--popup-container");
+  popup.setAttribute("aria-hidden","true");
+  popup.remove();
+}
